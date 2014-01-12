9fede6a4ab9eee8f8c0d2c2cbfc79e99a406cc39
Danny Blackstock <dannyblackstock@gmail.com>
2014-01-11 21:18:30 -0800

    first commit

---
+++ dblackst/includes/header.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="description" content="The School of Interactive Art and Technology's website for connecting past, present, and future  students.">
+    <meta name="author" content="Danny Blackstock">
+    <title>SFU SIAT Outreach</title>
+    <link rel="shortcut icon" href="img/favicon.ico" />
+</head>
+
+<body>
---
+++ dblackst/index.php
@@ -0,0 +1,13 @@
+<!-- HTML header, title, body tags, etc -->
+<?php require("includes/header.php"); ?>
+
+<!-- Main menu bar for all pages -->
+<div id="main-menu-bar">
+    <div id="main-logo">SIAT SFU</div>
+    <div id="sign-up-link">Don't have an account yet? <a href="sign_up.php">Sign up!</a></div>
+    <div id="log-in-button">
+        <a href="log_in.php">Log in</a>
+    </div>
+</div>
+
+<?php require("includes/footer.php"); ?>
