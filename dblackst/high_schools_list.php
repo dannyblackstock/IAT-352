3382800585e3ecbd3168f0c2a5483cf327bcc421
Danny Blackstock <dannyblackstock@gmail.com>
2014-01-18 21:38:43 -0800

    Some error checking

--- dblackst/high_schools_list.php
+++ dblackst/high_schools_list.php
@@ -26,30 +26,32 @@
                 return strnatcasecmp($a[5], $b[5]);
             }
 
-            // array to hold users when all fields are separated and sorted
-            $usersSortedArray = array();
+            if (filesize("user_info.txt") > 0) {
+                // array to hold users when all fields are separated and sorted
+                $usersSortedArray = array();
 
-            // load info file for all users
-            $userFile = file_get_contents("user_info.txt");
+                // load info file for all users
+                $userFile = file_get_contents("user_info.txt");
 
-            // create an array of each user by splitting it every new line
-            $usersArray = explode("\n", $userFile);
+                // create an array of each user by splitting it every new line
+                $usersArray = explode("\n", $userFile);
 
-            foreach ($usersArray as $user) {
-                // split each user into an array of fields and add it to an array
-                $user = explode(" | ", $user);
-                array_push($usersSortedArray, $user);
-            }
+                foreach ($usersArray as $user) {
+                    // split each user into an array of fields and add it to an array
+                    $user = explode(" | ", $user);
+                    array_push($usersSortedArray, $user);
+                }
 
-            // sort the array of arrays alphabetically by last name
-            usort($usersSortedArray, 'compare_lastname');
+                // sort the array of arrays alphabetically by last name
+                usort($usersSortedArray, 'compare_lastname');
 
-            // print the sorted array
-            foreach($usersSortedArray as $user){
-                echo "<tr><td class=\"profile-picture-cell\"><img src=\"img/user_icon.png\" alt=\"Placeholder user photo\" width=\"50\" height=\"50\"</td>";
-                echo "<td class=\"user-name-cell\">".$user[1]."</td>";
-                echo "<td class=\"high-school-name-cell\">".$user[5]."</td>";
-                echo "</tr>";
+                // print the sorted array
+                foreach($usersSortedArray as $user){
+                    echo "<tr><td class=\"profile-picture-cell\"><img src=\"img/user_icon.png\" alt=\"Placeholder user photo\" width=\"50\" height=\"50\"</td>";
+                    echo "<td class=\"user-name-cell\">".$user[1]."</td>";
+                    echo "<td class=\"high-school-name-cell\">".$user[5]."</td>";
+                    echo "</tr>";
+                }
             }
         ?>
     </table>
--- dblackst/index.php
+++ dblackst/index.php
@@ -26,30 +26,32 @@
                 return strnatcasecmp($a[1], $b[1]);
             }
 
-            // array to hold users when all fields are separated and sorted
-            $usersSortedArray = array();
+            if (filesize("user_info.txt") > 0) {
+                // array to hold users when all fields are separated and sorted
+                $usersSortedArray = array();
 
-            // load info file for all users
-            $userFile = file_get_contents("user_info.txt");
+                // load info file for all users
+                $userFile = file_get_contents("user_info.txt");
 
-            // create an array of each user by splitting it every new line
-            $usersArray = explode("\n", $userFile);
+                // create an array of each user by splitting it every new line
+                $usersArray = explode("\n", $userFile);
 
-            foreach ($usersArray as $user) {
-                // split each user into an array of fields and add it to an array
-                $user = explode(" | ", $user);
-                array_push($usersSortedArray, $user);
-            }
+                foreach ($usersArray as $user) {
+                    // split each user into an array of fields and add it to an array
+                    $user = explode(" | ", $user);
+                    array_push($usersSortedArray, $user);
+                }
 
-            // sort the array of arrays alphabetically by last name
-            usort($usersSortedArray, 'compare_name');
+                // sort the array of arrays alphabetically by last name
+                usort($usersSortedArray, 'compare_name');
 
-            // print the sorted array
-            foreach($usersSortedArray as $user){
-                echo "<tr><td class=\"profile-picture-cell\"><img src=\"img/user_icon.png\" alt=\"Placeholder user photo\" width=\"50\" height=\"50\"</td>";
-                echo "<td class=\"user-name-cell\"><a href=\"user.php?id=".$user[0]."\">".$user[1]."</a></td>";
-                echo "<td class=\"high-school-name-cell\">".$user[5]."</td>";
-                echo "</tr>";
+                // print the sorted array
+                foreach($usersSortedArray as $user){
+                    echo "<tr><td class=\"profile-picture-cell\"><img src=\"img/user_icon.png\" alt=\"Placeholder user photo\" width=\"50\" height=\"50\"</td>";
+                    echo "<td class=\"user-name-cell\"><a href=\"user.php?id=".$user[0]."\">".$user[1]."</a></td>";
+                    echo "<td class=\"high-school-name-cell\">".$user[5]."</td>";
+                    echo "</tr>";
+                }
             }
         ?>
     </table>
