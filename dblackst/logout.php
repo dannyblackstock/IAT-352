d6d3d7fbf30ce854935ce2e33dfb4d46c84de639
Danny Blackstock <dannyblackstock@gmail.com>
2014-02-16 23:34:44 -0800

    Simplify logout

--- dblackst/logout.php
+++ dblackst/logout.php
@@ -1,5 +1,4 @@
 <?php
-require_once("includes/header.php");
 
 if (session_status() == PHP_SESSION_NONE) {
     session_start();
@@ -8,26 +7,16 @@ if (session_status() == PHP_SESSION_NONE) {
 if (isset($_SESSION['valid_member']) || isset($_SESSION['valid_visitor'])) {
     unset($_SESSION['valid_member']);
     unset($_SESSION['valid_visitor']);
-    $loggedOut = True;
-} else {
-    $loggedOut = False;
-}
-
-//Main menu bar for all pages
-require_once("includes/main_menu_bar_https.php");
-
-if ($loggedOut == True) {
     echo "<h1>You have been logged out.</h1>";
-}
-else if ($loggedOut == False) {
+} else {
     echo "<h1>You were not logged in.</h1>";
 }
 
-echo "<a href=\"index.php\">Index</a>";
+header('Location:'.$_SESSION['callback_URL']);
 
 //session_destroy();
 
 // echo '<p>Menu: <a href="protectedPage1.php">Page 1</a>, 
-// 			<a href="protectedPage2.php">Page 2</a></p>';
-	
- require("includes/footer.php"); ?>
+//          <a href="protectedPage2.php">Page 2</a></p>';
+    
+?>
