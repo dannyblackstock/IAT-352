8e89889821585c25d9f5ca4b492389cd4abe7e86
Danny Blackstock <dannyblackstock@gmail.com>
2014-03-06 10:25:44 -0800

    Add tutorial 8

---
+++ tutorial8/twitter_config.php
@@ -0,0 +1,6 @@
+<?php
+	$consumer_key = "sljQznsDb4vbMJfDgpNkA";
+	$consumer_secret = "d88t8V7FQ9R0T5fJL97R9FJIvSsUJnAxZbuTmfI34";
+	$access_token = "15258069-1tASfSDfE3AOH7EAdqQRvdZNAiZ6YR5M1yjMM4a5j";
+	$access_token_secret = "VW6hrGgvt64oMDAUxduvrTAX8Xznl8iXYuxtcosL3uUZb";
+?>
